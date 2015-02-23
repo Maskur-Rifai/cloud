b03a320ac91f1782355641382dfb558e3c63b9f6
Maskur-Rifai <trensify4@gmail.com>
2015-02-23 10:02:55 +0700

    file koneksi

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+	$dbhost = 'localhost:3036';
+$dbuser = 'root';
+$dbpass = '';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+?>
