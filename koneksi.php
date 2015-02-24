24fca7d9fb73b48c88296222f2ca0b37096cf8fb
Maskur-Rifai <trensify4@gmail.com>
2015-02-24 09:33:12 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,15 @@
 <?php
 	$dbhost = 'localhost:3036';
-$dbuser = 'root';
-$dbpass = '';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-if(! $conn )
-{
-  die('Could not connect: ' . mysql_error());
-}
+	$dbuser = 'root';
+	$dbpass = '';
+	$dbname='sid';
+	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+	if(! $conn )
+	{
+	  die('Could not connect: ' . mysql_error());
+	}
+	$conectdb=mysql_select_db($dbname,$conn);
+	if(!$conectdb){
+		 die('Could not connect: ' . mysql_error());
+		}
 ?>
